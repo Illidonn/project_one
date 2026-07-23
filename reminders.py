229afe837fb77982683a8044decdb8e48e156563
illidonn <63295015+Illidonn@users.noreply.github.com>
2026-07-23 19:41:22 +0300

    Add Error handling in reminders-1

--- reminders.py
+++ reminders.py
@@ -9,14 +9,20 @@ async def run_reminders(bot):
     while True:
         for booking in get_bookings_for_day_reminder():
             booking_id, user_id, user_name, _, b_time = booking
-            text = day_reminder_text(user_name, b_time)
-            await bot.send_message(user_id, text)
-            mark_day_reminder_sent(booking_id)
+            try:
+                text = day_reminder_text(user_name, b_time)
+                await bot.send_message(user_id, text)
+                mark_day_reminder_sent(booking_id)
+            except Exception:
+                logging.exception("Не удалось отправить дневное напоминание, booking_id=%s", booking_id)
 
         for booking in get_bookings_for_hour_reminder():
             booking_id, user_id, user_name, _, b_time = booking
-            text = hour_reminder_text(user_name, b_time)
-            await bot.send_message(user_id, text)
-            mark_hour_reminder_sent(booking_id)
+            try:
+                text = hour_reminder_text(user_name, b_time)
+                await bot.send_message(user_id, text)
+                mark_hour_reminder_sent(booking_id)
+            except Exception:
+                logging.exception("Не удалось отправить часовое напоминание, booking_id=%s", booking_id)
 
         await asyncio.sleep(600)
