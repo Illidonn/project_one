300b0910f9bc9e55ba421dddf2516b4b681cd84e
illidonn <63295015+Illidonn@users.noreply.github.com>
2026-07-23 19:39:03 +0300

    Add Error handling in reminders

--- keyboards_texts.py
+++ keyboards_texts.py
@@ -1,16 +1,30 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from datetime import date
 import html
+
+
+DAYS_RU = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+
 
 def successful_booked_text(user_name):
     return f"{html.escape(user_name)}, запись подтверждена!"
 
 unsuccessful_booked_text = "К сожалению, эта запись уже занята."
 
-def build_slots_keyboard(free_slots_list, date):
+def build_slots_keyboard(free_slots_list, iso_date):
     builder = InlineKeyboardBuilder()
 
     for slot in free_slots_list:
-        builder.button(text=f"{slot}:00", callback_data=f"book|{slot}|{date}")
+        builder.button(text=slot, callback_data=f"book|{slot}|{iso_date}")
+    builder.adjust(3) 
+    return builder.as_markup()
+
+def build_dates_keyboard(free_days_list):
+    builder = InlineKeyboardBuilder()
+
+    for d in free_days_list:
+        current = date.fromisoformat(d)
+        builder.button(text=f"{DAYS_RU[current.weekday()]} {current.strftime('%d/%m')}", callback_data=f"day|{d}")
     builder.adjust(3) 
     return builder.as_markup()
 
